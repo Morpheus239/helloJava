871a65cff0a01ad91debc83612f552e23dc7fb41
Michael <user@mail>
2012-10-25 13:44:47 +0200

    Einfach mal so

--- HelloJava/src/HelloJava.java
+++ HelloJava/src/HelloJava.java
@@ -1,10 +1,17 @@
 
 public class HelloJava {
 
+	
+	//TODO Main Klasse ändern
 	public static void main(String[] args) {
 		System.out.println("Hello Java");
 		System.out.println("Commit 2");
 	}
+	
+	public void testname() throws Exception {
+		
+	}
+	
 	
 	
 }
